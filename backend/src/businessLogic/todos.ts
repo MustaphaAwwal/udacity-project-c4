e0d321eb418914b7b789400edc2d2c2594e99195
Mustapha Awwal <awwalmustapha41@gmail.com>
2023-03-05 03:05:43 +0100

    done with the project

---
+++ backend/src/businessLogic/todos.ts
@@ -0,0 +1,72 @@
+import { TodosAccess } from '../dataLayer/todosAcess'
+import { AttachmentUtils } from '../helpers/attachmentUtils';
+import { TodoItem } from '../models/TodoItem'
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import { createLogger } from '../utils/logger'
+import * as uuid from 'uuid'
+import { TodoUpdate } from '../models/TodoUpdate';
+//import * as createError from 'http-errors'
+
+// TODO: Implement businessLogic
+
+const logger = createLogger('TodoAccess')
+const attachmentUtils = new AttachmentUtils()
+const todosAccess = new TodosAccess()
+
+
+// get todos function
+export async function getTodosForUser(userId: string): Promise<TodoItem[]> {
+    logger.info('Get todos function called')
+    return todosAccess.getAllTodos(userId)
+}
+
+export async function createTodo(
+    newTodo: CreateTodoRequest,
+    userId: string
+    ): Promise<TodoItem> {
+        logger.info('create todo function called')    
+
+        const todoId = uuid.v4()
+        const createdAt = new Date().toISOString()
+        const s3AttachmentUrl = attachmentUtils.getAttachmentUrl(todoId)
+        const newItem = {
+            userId,
+            todoId,
+            createdAt,
+            done: false,
+            attachmentUrl: s3AttachmentUrl,
+            ...newTodo
+        }
+
+        return await todosAccess.createTodoItem(newItem)
+}
+
+// write update todo function
+export async function updateTodo(
+    todoId: string,
+    todoUpdate: UpdateTodoRequest,
+    userId: string
+): Promise<TodoUpdate> {
+    logger.info('Update todo function called')
+    return await todosAccess.updateTodoItem(todoId, userId, todoUpdate)
+    
+}
+
+// write delete function
+export async function deleteTodo(
+    todoId: string,
+    userId: string
+): Promise<string>{
+    logger.info('Delete function called')
+    return todosAccess.deleteTodoItem(todoId, userId)
+}
+
+export async function createAttachmentPresignedUrl(
+    todoId: string,
+    useId: string
+): Promise<string> {
+    logger.info('Create attachment function called', useId, todoId)
+    return attachmentUtils.getUploadUrl(todoId)
+    
+}
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'z1kp572qrd'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-ezs6q7wpu762ux4a.us.auth0.com',            // Auth0 domain
+  clientId: 'sJL8mrgovDl9fdEHANh3wy3Kx1QBSJsU',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
